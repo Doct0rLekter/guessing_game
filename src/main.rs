787d79a1ab24853a547798cf72afa841d075b509
Doct0rLekter <marikwinters@gmail.com>
2023-02-15 15:59:13 -0500

    Move gameplay loop to a separate function. Also, make it a loop.

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,41 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
+    let mut tries = 0;
+    
+    loop {
+        tries += 1;
+        if game_loop(secret_number) {
+            println!("You took {tries} tries to guess the number");
+            break;
+        }
+    }
+}
+
+fn game_loop(the_number: u32) -> bool {
+    let mut guess = String::new();
 
     print!("Please input your guess: ");
     // flush buffer
     stdout().flush().unwrap();
 
-    let mut guess = String::new();
-
     stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = guess.trim().parse().expect("Please type a positive integer!");
 
     println!("you guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    match guess.cmp(&the_number) {
+        Ordering::Less => {
+            println!("Too small!");
+            false
+        }
+        Ordering::Greater => {
+            println!("Too big!");
+            false
+        }
+        Ordering::Equal => {
+            println!("You win!");
+            true
+        }
     }
 }
