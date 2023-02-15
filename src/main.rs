c4acc1763c9e3943e27485483f6018c5aa67e202
Doct0rLekter <marikwinters@gmail.com>
2023-02-15 14:44:23 -0500

    Implement user input capture

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use std::io;
+// Make clippy pedantic
+#![warn(clippy::pedantic)]
+
+use std::io::{stdin, stdout, Write};
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    print!("Please input your guess: ");
+    // flush buffer
+    stdout().flush().unwrap();
+
+    let mut guess = String::new();
+
+    stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("you guessed: {guess}");
 }
---
+++ target/debug/deps/guessing_game-620a80a9ccc8ac8a.d
@@ -0,0 +1,8 @@
+/home/doct0rlekter/dev/rust/guessing_game/target/debug/deps/guessing_game-620a80a9ccc8ac8a.rmeta: src/main.rs Cargo.toml
+
+/home/doct0rlekter/dev/rust/guessing_game/target/debug/deps/guessing_game-620a80a9ccc8ac8a.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
